48525d61c03d91f5d36ed4301e1cf85a5e179413
Gonzalo Solera <gonzalo1796@gmail.com>
2016-10-13 00:00:15 +0200

    Implementing initialstate generator

--- src/LocalSearch/InitialStateFactory.java
+++ src/LocalSearch/InitialStateFactory.java
@@ -1,60 +1,34 @@
 package LocalSearch;
 
-import IA.Azamon.Oferta;
 import IA.Azamon.Paquete;
-import IA.Azamon.Paquetes;
-import IA.Azamon.Transporte;
-
-import java.util.Comparator;
 
 public class InitialStateFactory {
 
-    public static State generateInitialState(int n, int prop, int seed) {
-        Paquetes paquetes = new Paquetes(n, seed);
-        paquetes.sort(new Comparator<Paquete>() {
-            @Override
-            public int compare(Paquete a, Paquete b) {
-                if(a.getPrioridad() < b.getPrioridad()) {
-                    return 1;
-                } else if(a.getPrioridad() > b.getPrioridad()) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            }
-        });
-        Transporte ofertas = new Transporte(paquetes, prop, seed);
-        ofertas.sort(new Comparator<Oferta>() {
-            @Override
-            public int compare(Oferta a, Oferta b) {
-                if(a.getDias() < b.getDias()) {
-                    return 1;
-                } else if(a.getDias() > b.getDias()) {
-                    return -1;
-                } else {
-                    if(a.getPrecio() < b.getPrecio()) {
-                        return 1;
-                    } else if(a.getPrecio() > b.getPrecio()) {
-                        return -1;
-                    } else {
-                        return 0;
+    public static State generateInitialState(Problem problem) {
+        State state = new State(problem);
+        i_generate(0, state, problem);
+        return state;
+    }
+
+    private static boolean i_generate(int i, State state, Problem problem) {
+        if(i == problem.totalPackages()) {
+            System.out.println("hello");
+            state.print();
+            return true;
+        } else {
+            Paquete act = problem.getPackage(i);
+            for(int days = act.getPrioridad()*2+1; days >= 1; --days) {
+                for(int offerID : problem.offers[days-1]) {
+                    if(state.movePackage(i, offerID)) {
+                        boolean found = i_generate(i+1, state, problem);
+                        if(found) {
+                            return true;
+                        }
+                        state.offer_of_package[i] = -1;
                     }
                 }
             }
-        });
-        int[] distribution = new int[paquetes.size()];
-        int[] weights = new int[ofertas.size()];
-        for(int i = 0; i < weights.length; ++i) {
-            weights[i] = 0;
-        }
-        return new State();
-    }
-
-    private static void i_generate(int i, int j, int[] solution, int[]repart) {
-        if(i == solution.length) {
-            System.console().printf("hello");
-        } else {
-
+            return false;
         }
     }
 }
