c63293a0379ae97147fbed2fa46f296a18061494
daniconde <daniabc2@gmail.com>
2016-10-12 18:22:00 +0200

    Add State class

--- src/LocalSearch/State.java
+++ src/LocalSearch/State.java
@@ -1,4 +1,46 @@
 package LocalSearch;
 
+import IA.Azamon.Paquetes;
+import IA.Azamon.Transporte;
+
 public class State {
+
+    static private Paquetes packages;
+    static private Transporte offers;
+
+    // Contains the offer's index to which a package is assigned
+    private int[] offer_of_package;
+    // Contains the sum of weights of the packages assigned to an offer
+    private int[] weight_of_offer;
+
+    public static void setPackagesOffers(Paquetes packages, Transporte offers) {
+        State.packages = packages;
+        State.offers = offers;
+    }
+
+    public State() {
+        offer_of_package = new int[packages.size()];
+        weight_of_offer = new int[offers.size()];
+    }
+
+    public State(State from) {
+        this.offer_of_package = from.offer_of_package.clone();
+        this.weight_of_offer = from.weight_of_offer.clone();
+    }
+
+    public void setOfferOfPackage(int package_index, int offer_index) {
+        offer_of_package[package_index] = offer_index;
+    }
+
+    public int getOfferOfPackage(int package_index) {
+        return offer_of_package[package_index];
+    }
+
+    public void setWeightOfOffer(int offer_index, int weight) {
+        weight_of_offer[offer_index] = weight;
+    }
+
+    public int getWeightOfOffer(int offer_index) {
+        return weight_of_offer[offer_index];
+    }
 }
