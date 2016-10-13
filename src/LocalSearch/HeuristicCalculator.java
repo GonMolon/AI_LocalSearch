c01f9b241d183aeecc34eaab54ccbc5381a1e1eb
Gonzalo Solera <gonzalo1796@gmail.com>
2016-10-13 17:48:20 +0200

    Adding AIMA interface

---
+++ src/LocalSearch/HeuristicCalculator.java
@@ -0,0 +1,10 @@
+package LocalSearch;
+
+import aima.search.framework.HeuristicFunction;
+
+public class HeuristicCalculator implements HeuristicFunction {
+    @Override
+    public double getHeuristicValue(Object state) {
+        return ((State) state).getCost();
+    }
+}
---
+++ src/LocalSearch/SuccesorGenerator.java
@@ -0,0 +1,40 @@
+package LocalSearch;
+
+import aima.search.framework.SuccessorFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuccesorGenerator implements SuccessorFunction {
+
+    @Override
+    public List getSuccessors(Object o) {
+        State state = (State) o;
+        Problem problem = state.getProblem();
+        ArrayList<State> successors = new ArrayList<>();
+        State nextState = new State(state);
+        for(int fromID = 0; fromID < problem.totalPackages(); ++fromID) {
+            for(int toID = 0; toID < problem.totalPackages(); ++toID) {
+                if(fromID != toID) {
+                    if(nextState.swapPackage(fromID, toID)) {
+                        successors.add(nextState);
+                        nextState = new State(state);
+                    }
+                }
+            }
+        }
+        for(int fromID = 0; fromID < problem.totalPackages(); ++fromID) {
+            for(int days = problem.getPackage(fromID).getPrioridad()*2+1; days >= 1; --days) {
+                for(int toID = 0; toID < problem.offers[days-1].length; ++toID) {
+                    if(state.offer_of_package[fromID] != toID) {
+                        if(nextState.movePackage(fromID, toID)) {
+                            successors.add(nextState);
+                            nextState = new State(state);
+                        }
+                    }
+                }
+            }
+        }
+        return successors;
+    }
+}
