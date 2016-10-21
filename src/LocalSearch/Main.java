0b5a6577e2d518705292e0e61bcaeb485f1bc590
Gonzalo Solera <gonzalo1796@gmail.com>
2016-10-21 16:03:51 +0200

    some changes

--- src/LocalSearch/Main.java
+++ src/LocalSearch/Main.java
@@ -8,27 +8,30 @@ import aima.search.informed.SimulatedAnnealingSearch;
 
 public class Main {
     public static void main(String[] args) {
+        if(args.length != 12) {
+            return;
+        }
         int out = Integer.parseInt(args[0]);
         int seed = Integer.parseInt(args[1]);
         int n = Integer.parseInt(args[2]);
+        double prop = Double.parseDouble(args[3]);
         int fel = Integer.parseInt(args[4]);
         int ope = Integer.parseInt(args[5]);
+        int gen = Integer.parseInt(args[6]);
         int alg = Integer.parseInt(args[7]);
         int ite = Integer.parseInt(args[8]);
         int ste = Integer.parseInt(args[9]);
         int k = Integer.parseInt(args[10]);
-        double prop = Double.parseDouble(args[3]), lam = Double.parseDouble(args[11]);
-        int gen = Integer.parseInt(args[6]);
-        Statement statement;
+        double lam = Double.parseDouble(args[11]);
+
         State initialState;
         State.set_Happiness(fel);
         if(gen == 0) {
             Statement.generateStatement(n, prop, seed, SortMode.OPTIMUM);
-            initialState = InitialStateFactory.generateInitialState(Statement.getStatement(),  true);
         } else {
             Statement.generateStatement(n, prop, seed, SortMode.RANDOM);
-            initialState = InitialStateFactory.generateInitialState(Statement.getStatement(),  false);
         }
+        initialState = InitialStateFactory.generateInitialState(Statement.getStatement(),  gen == 0);
         Search search;
         Problem problem;
         if(alg == 0) {
