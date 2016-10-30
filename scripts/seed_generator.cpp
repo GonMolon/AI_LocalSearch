0c97b18b65fe77d9bc1cf0036b3953730b7cce79
daniconde <daniabc2@gmail.com>
2016-10-30 18:37:54 +0100

    Fix seed generator

--- scripts/seed_generator.cpp
+++ scripts/seed_generator.cpp
@@ -2,11 +2,12 @@
 #include <time.h>
 #include <stdlib.h>
 #include <cstdlib>
+#include <unistd.h>
 using namespace std;
 
 int main(int argc, char* argv[]) 
 {
-    srand(time(NULL));
+    srand(time(NULL)*getpid());
     int n = atoi(argv[1]);
     for (int i = 0; i < n; ++i) {
         int randnumber = rand();
